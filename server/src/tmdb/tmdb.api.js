b301194ab932e48847102a1408f6c52697af9a99
m13ks <korzhanm@gmail.com>
2023-06-29 12:47:20 +0300

    create moviesapp & add server part

---
+++ server/src/tmdb/tmdb.api.js
@@ -0,0 +1,33 @@
+import axiosClient from "../axios/axios.client"
+import tmdbEndpoints from "./tmdb.endpoints"
+
+const tmdbApi = {
+  mediaList: async ({ mediaType, mediaCategory, page }) =>
+    await axiosClient.get(
+      tmdbEndpoints.mediaList({ mediaType, mediaCategory, page })
+    ),
+  mediaDetail: async ({ mediaType, mediaId }) =>
+    await await axiosClient.get(
+      tmdbEndpoints.mediaDetail({ mediaType, mediaId })
+    ),
+  mediaGenres: async ({ mediaType }) =>
+    await axiosClient.get(tmdbEndpoints.mediaGenres({ mediaType })),
+  mediaCredits: async ({ mediaType, mediaId }) =>
+    await axiosClient.get(tmdbEndpoints.mediaCredits({ mediaType, mediaId })),
+  mediaVideos: async ({ mediaType, mediaId }) =>
+    await axiosClient.get(tmdbEndpoints.mediaVideos({ mediaType, mediaId })),
+  mediaImages: async ({ mediaType, mediaId }) =>
+    await axiosClient.get(tmdbEndpoints.mediaImages({ mediaType, mediaId })),
+  mediaRecommend: async ({ mediaType, mediaId }) =>
+    await axiosClient.get(tmdbEndpoints.mediaRecommend({ mediaType, mediaId })),
+  mediaSearch: async ({ mediaType, query, page }) =>
+    await axiosClient.get(
+      tmdbEndpoints.mediaSearch({ mediaType, query, page })
+    ),
+  personDetails: async ({ personId }) =>
+    await axiosClient.get(tmdbEndpoints.personDetails({ personId })),
+  personMedias: async ({ personId }) =>
+    await axiosClient.get(tmdbEndpoints.personMedias({ personId })),
+}
+
+export default tmdbApi
